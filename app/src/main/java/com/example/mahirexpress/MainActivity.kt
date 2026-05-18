8d9c4e6caee06c58e94280a37e2b3a0deb379f93
mahir <sajidhasanmahir2003@gmail.com>
2026-05-18 23:24:20 +0600

    work fine

--- app/src/main/java/com/example/mahirexpress/MainActivity.kt
+++ app/src/main/java/com/example/mahirexpress/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.mahirexpress
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import com.example.mahirexpress.databinding.ActivityMainBinding
 import com.example.mahirexpress.fragments.BookingsFragment
@@ -17,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        applyWindowInsets()
         setSupportActionBar(binding.toolbar)
 
         val targetFragment = intent.getStringExtra("target_fragment")
@@ -50,5 +54,21 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragment)
             .commit()
+    }
+
+    private fun applyWindowInsets() {
+        val appBarTopPadding = binding.appBarLayout.paddingTop
+        val bottomNavBottomPadding = binding.bottomNavigation.paddingBottom
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, windowInsets ->
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            binding.appBarLayout.updatePadding(top = appBarTopPadding + systemBars.top)
+            binding.bottomNavigation.updatePadding(bottom = bottomNavBottomPadding + systemBars.bottom)
+
+            windowInsets
+        }
+
+        ViewCompat.requestApplyInsets(binding.root)
     }
 }
